d257663ce5bf0e356932d7f66860b8abec532cc1
Fatima Al Mahfoodh <fatimahalmahfoodh2000@hotmail.com>
2023-08-22 19:28:48 +0300

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import BannerBackground from "../Assets/bg1.svg";
+import BannerImage from "../Assets/1.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favorite Coffee Prepared The Way You Like It
+          </h1>
+          <p className="primary-text">
+            
+            Professional baristas prepare your coffee using freshly grounded beans so you can enjoy your coffee 
+            in any time you like it.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
