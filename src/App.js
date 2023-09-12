024a60c04a57599a257d95647eef5147c7884379
Fatima Al Mahfoodh <fatimahalmahfoodh2000@hotmail.com>
2023-09-12 14:46:54 +0300

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 import Home from "./Components/Home";
+import Testimonial from './Components/Testimonial';
+import Work from "./Components/Work";
 function App() {
   return (
     <div className="App">
       <Home />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
